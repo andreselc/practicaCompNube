7fab8c89154e6ef22ba3c2f0febdb2d3c15e9f5d
Andrés <95057542+andreselc@users.noreply.github.com>
2024-05-12 22:46:17 -0400

    New Change in DTO

--- src/apiFolder/dtos/creacionObjeto.dto.ts
+++ src/apiFolder/dtos/creacionObjeto.dto.ts
@@ -1,9 +1,11 @@
 import {IsEmail, IsString} from "class-validator";
 
 export class creacionObjetoDto{
-  @IsEmail()
-  email: string;
-  
+   
   @IsString()
   name: string;
+
+  @IsEmail({}, { each: true }) 
+  emails: string[]; 
+
 }
