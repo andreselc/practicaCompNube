70a0112354763a7a2f73bceb2d2c0581858aa7c0
Andrés <95057542+andreselc@users.noreply.github.com>
2024-05-18 17:31:48 -0400

    ORM CONFIG Improved

--- orm.config.ts
+++ orm.config.ts
@@ -2,18 +2,20 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { config } from 'dotenv';
 config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const ormconfig: TypeOrmModuleOptions = {
   type: 'postgres',
   username: 'user',
   password: 'dummypass',
-  host: 'codrr_pg',
+  host: isProduction ? process.env.DATABASE_HOST_PROD : process.env.DATABASE_HOST_DEV,
   port: 5432,
   database: 'nube',
   entities: ['dist/src/apiFolder/*.entity.js'],
   migrations: ['dist/src/database/migrations/*.js'],
-  synchronize: true,
+  synchronize: false,
   autoLoadEntities: true,
-  migrationsRun: true,
+  migrationsRun: isProduction,
 };
 
 export default ormconfig;
