a2e6586e7128f168e93c91760ab59834c2fd3104
Andrés <95057542+andreselc@users.noreply.github.com>
2024-05-18 18:58:03 -0400

    Fixing a bug in Orm.Config.ts

--- orm.config.ts
+++ orm.config.ts
@@ -2,20 +2,18 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { config } from 'dotenv';
 config();
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 const ormconfig: TypeOrmModuleOptions = {
   type: 'postgres',
   username: 'user',
   password: 'dummypass',
-  host: isProduction ? process.env.DATABASE_HOST_PROD : process.env.DATABASE_HOST_DEV,
+  host: 'codrr_pg',
   port: 5432,
   database: 'nube',
   entities: ['dist/src/apiFolder/*.entity.js'],
   migrations: ['dist/src/database/migrations/*.js'],
-  synchronize: false,
+  synchronize: true,
   autoLoadEntities: true,
-  migrationsRun: isProduction,
+  migrationsRun: true,
 };
 
 export default ormconfig;
